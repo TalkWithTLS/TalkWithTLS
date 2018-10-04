d81a258d13a991c7fbf9c137748f7a70394b4397
rashok <rashok.svks@gmail.com>
2018-10-04 16:41:56 +0530

    Add TLS1.2 sample code

---
+++ tls12_client.c
@@ -0,0 +1,148 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <errno.h>
+
+#include "openssl/crypto.h"
+#include "openssl/ssl.h"
+
+#define CAFILE1 "./certs/ECC_Prime256_Certs/rootcert.pem"
+#define SERVER_IP "127.0.0.1"
+#define SERVER_PORT 7788
+
+int do_tcp_connection(const char *server_ip, uint16_t port)
+{
+    struct sockaddr_in serv_addr;
+    int fd;
+    int ret;
+
+    fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (fd < 0) {
+        printf("Socket creation failed\n");
+        return -1;
+    }
+    printf("Client fd=%d created\n", fd);
+
+    serv_addr.sin_family = AF_INET;
+    if (inet_aton(server_ip, &serv_addr.sin_addr) == 0) {
+        printf("inet_aton failed\n");
+        goto err_handler;
+    }
+    serv_addr.sin_port = htons(port);
+
+    ret = connect(fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+    if (ret) {
+        printf("Connect failed, errno=%d\n", errno);
+        goto err_handler;
+    }
+    
+    printf("TLS connection succeeded, fd=%d\n", fd);
+    return fd;
+err_handler:
+    close(fd);
+    return -1;
+}
+
+SSL_CTX *create_context()
+{
+    SSL_CTX *ctx;
+
+    ctx = SSL_CTX_new(TLS_client_method());
+    if (!ctx) {
+        printf("SSL ctx new failed\n");
+        return NULL;
+    }
+
+    printf("SSL context created\n");
+
+    if (SSL_CTX_load_verify_locations(ctx, CAFILE1, NULL) != 1) {
+        printf("Load CA cert failed\n");
+        goto err_handler;
+    }
+
+    printf("Loaded cert %s on context\n", CAFILE1);
+
+    SSL_CTX_set_verify(ctx, SSL_VERIFY_PEER, NULL);
+    SSL_CTX_set_verify_depth(ctx, 5);
+    SSL_CTX_set_options(ctx, SSL_OP_NO_TLSv1_3);
+
+    printf("SSL context configurations completed\n");
+
+    return ctx;
+err_handler:
+    SSL_CTX_free(ctx);
+    return NULL;
+}
+
+SSL *create_ssl_object(SSL_CTX *ctx)
+{
+    SSL *ssl;
+    int fd;
+
+    fd = do_tcp_connection(SERVER_IP, SERVER_PORT);
+    if (fd < 0) {
+        printf("TCP connection establishment failed\n");
+        return NULL;
+    }
+
+    ssl = SSL_new(ctx);
+    if (!ssl) {
+        printf("SSL object creation failed\n");
+        return NULL; 
+    }
+
+    SSL_set_fd(ssl, fd);
+
+    printf("SSL object creation finished\n");
+
+    return ssl;
+}
+
+int tls12_client()
+{
+    SSL_CTX *ctx;
+    SSL *ssl = NULL;
+    int fd;
+    int ret;
+
+    ctx = create_context();
+    if (!ctx) {
+        return -1;
+    }
+
+    ssl = create_ssl_object(ctx);
+    if (!ssl) {
+        goto err_handler;
+    }
+
+    ret = SSL_connect(ssl); 
+    if (ret != 1) {
+        printf("SSL connect failed%d\n", ret);
+        goto err_handler;
+    }
+
+    printf("SSL connect succeeded\n");
+    SSL_free(ssl);
+    SSL_CTX_free(ctx);
+
+    return 0;
+err_handler:
+    if (ssl) {
+        SSL_free(ssl);
+    }
+    SSL_CTX_free(ctx);
+    return -1;
+}
+
+int main()
+{
+    printf("OpenSSL version: %s, %s\n", OpenSSL_version(OPENSSL_VERSION), OpenSSL_version(OPENSSL_BUILT_ON));
+    if (tls12_client()) {
+        printf("TLS12 client connection failed\n");
+    }
+}
---
+++ tls12_server.c
@@ -0,0 +1,185 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <errno.h>
+
+#include "openssl/crypto.h"
+#include "openssl/ssl.h"
+
+#define SERVER_CERT_FILE "./certs/ECC_Prime256_Certs/serv_cert.pem"
+#define SERVER_KEY_FILE "./certs/ECC_Prime256_Certs/serv_key.der"
+#define EC_CURVE_NAME NID_X9_62_prime256v1
+
+#define SERVER_IP "127.0.0.1"
+#define SERVER_PORT 7788
+
+int do_tcp_accept(const char *server_ip, uint16_t port)
+{
+    struct sockaddr_in addr;
+    struct sockaddr_in peeraddr;
+    socklen_t peerlen = sizeof(peeraddr);
+    int lfd, cfd;
+    int ret;
+
+    lfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (lfd < 0) {
+        printf("Socket creation failed\n");
+        return -1;
+    }
+
+    addr.sin_family = AF_INET;
+    if (inet_aton(server_ip, &addr.sin_addr) == 0) {
+        printf("inet_aton failed\n");
+        goto err_handler;
+    }
+    addr.sin_port = htons(port);
+
+    ret = bind(lfd, (struct sockaddr *)&addr, sizeof(addr));
+    if (ret) {
+        printf("bind failed\n");
+        goto err_handler;
+    }
+
+    ret = listen(lfd, 5);
+    if (ret) {
+        printf("listen failed\n");
+        goto err_handler;
+    }
+
+    printf("Waiting for TCP connection from client...\n");
+    cfd = accept(lfd, (struct sockaddr *)&peeraddr, &peerlen);
+    if (cfd < 0) {
+        printf("accept failed, errno=%d\n", errno);
+        goto err_handler;
+    }
+
+    printf("TCP connection accepted fd=%d\n", cfd);
+    close(lfd);
+    return cfd;
+err_handler:
+    close(lfd);
+    return -1;
+}
+
+SSL_CTX *create_context()
+{
+    SSL_CTX *ctx;
+
+    ctx = SSL_CTX_new(TLS_server_method());
+    if (!ctx) {
+        printf("SSL ctx new failed\n");
+        return NULL;
+    }
+
+    printf("SSL context created\n");
+
+    if (SSL_CTX_use_certificate_file(ctx, SERVER_CERT_FILE, SSL_FILETYPE_PEM) != 1) {
+        printf("Load Server cert %s failed\n", SERVER_CERT_FILE);
+        goto err_handler;
+    }
+
+    printf("Loaded server cert %s on context\n", SERVER_CERT_FILE);
+
+    if (SSL_CTX_use_PrivateKey_file(ctx, SERVER_KEY_FILE, SSL_FILETYPE_ASN1) != 1) {
+        printf("Load Server key %s failed\n", SERVER_KEY_FILE);
+        goto err_handler;
+    }
+
+    printf("Loaded server key %s on context\n", SERVER_KEY_FILE);
+
+    SSL_CTX_set_options(ctx, SSL_OP_NO_TLSv1_3);
+    printf("SSL context configurations completed\n");
+
+    return ctx;
+err_handler:
+    SSL_CTX_free(ctx);
+    return NULL;
+}
+
+SSL *create_ssl_object(SSL_CTX *ctx)
+{
+    SSL *ssl;
+    EC_KEY *ecdh;
+    int fd;
+
+    fd = do_tcp_accept(SERVER_IP, SERVER_PORT);
+    if (fd < 0) {
+        printf("TCP connection establishment failed\n");
+        return NULL;
+    }
+
+    ssl = SSL_new(ctx);
+    if (!ssl) {
+        printf("SSL object creation failed\n");
+        return NULL;
+    }
+
+    SSL_set_fd(ssl, fd);
+
+    ecdh = EC_KEY_new_by_curve_name(EC_CURVE_NAME);
+    if (!ecdh) {
+        printf("ECDH generation failed\n");
+        goto err_handler;
+    }
+
+    SSL_set_tmp_ecdh(ssl, ecdh);
+    EC_KEY_free(ecdh);
+    ecdh = NULL;
+
+    printf("SSL object creation finished\n");
+
+    return ssl;
+err_handler:
+    SSL_free(ssl);
+    return NULL;
+}
+
+int tls12_server()
+{
+    SSL_CTX *ctx;
+    SSL *ssl = NULL;
+    int fd;
+    int ret;
+
+    ctx = create_context();
+    if (!ctx) {
+        return -1;
+    }
+
+    ssl = create_ssl_object(ctx);
+    if (!ssl) {
+        goto err_handler;
+    }
+
+    ret = SSL_accept(ssl); 
+    if (ret != 1) {
+        printf("SSL accept failed%d\n", ret);
+        goto err_handler;
+    }
+
+    printf("SSL accept succeeded\n");
+    SSL_free(ssl);
+    SSL_CTX_free(ctx);
+
+    return 0;
+err_handler:
+    if (ssl) {
+        SSL_free(ssl);
+    }
+    SSL_CTX_free(ctx);
+    return -1;
+}
+
+int main()
+{
+    printf("OpenSSL version: %s, %s\n", OpenSSL_version(OPENSSL_VERSION), OpenSSL_version(OPENSSL_BUILT_ON));
+    if (tls12_server()) {
+        printf("TLS12 server connection failed\n");
+    }
+}
+
