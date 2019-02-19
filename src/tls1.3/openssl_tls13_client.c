b4e578a095f57a4ba20c80abc6bf1c8b3f23e2f7
raja-ashok <rashok.svks@gmail.com>
2019-02-19 11:37:46 +0530

    Add testcase for TLS1.3 in OpenSSL

---
+++ src/tls1.3/openssl_tls13_client.c
@@ -0,0 +1,161 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <errno.h>
+
+#include "openssl/crypto.h"
+#include "openssl/ssl.h"
+
+#include "test_common.h"
+
+#define CAFILE1 "./certs/ECC_Prime256_Certs/rootcert.pem"
+
+int g_kexch_groups[] = {
+    NID_ffdhe2048,
+    NID_ffdhe3072,
+    NID_ffdhe4096,
+    NID_ffdhe6144,
+    NID_ffdhe8192
+};
+
+SSL_CTX *create_context()
+{
+    SSL_CTX *ctx;
+
+    ctx = SSL_CTX_new(TLS_client_method());
+    if (!ctx) {
+        printf("SSL ctx new failed\n");
+        return NULL;
+    }
+
+    printf("SSL context created\n");
+
+    if (SSL_CTX_load_verify_locations(ctx, CAFILE1, NULL) != 1) {
+        printf("Load CA cert failed\n");
+        goto err_handler;
+    }
+
+    printf("Loaded cert %s on context\n", CAFILE1);
+
+    SSL_CTX_set_verify(ctx, SSL_VERIFY_PEER, NULL);
+    SSL_CTX_set_verify_depth(ctx, 5);
+
+    printf("SSL context configurations completed\n");
+
+    return ctx;
+err_handler:
+    SSL_CTX_free(ctx);
+    return NULL;
+}
+
+SSL *create_ssl_object(SSL_CTX *ctx)
+{
+    SSL *ssl;
+    int fd;
+
+    fd = do_tcp_connection(SERVER_IP, SERVER_PORT);
+    if (fd < 0) {
+        printf("TCP connection establishment failed\n");
+        return NULL;
+    }
+
+    ssl = SSL_new(ctx);
+    if (!ssl) {
+        printf("SSL object creation failed\n");
+        return NULL; 
+    }
+
+    SSL_set_fd(ssl, fd);
+
+    if (SSL_set1_groups(ssl, g_kexch_groups, sizeof(g_kexch_groups)/sizeof(g_kexch_groups[0])) != 1) {
+        printf("Set Groups failed\n");
+        goto err_handler;
+    }
+
+    printf("SSL object creation finished\n");
+
+    return ssl;
+err_handler:
+    SSL_free(ssl);
+    return NULL;
+}
+
+int do_data_transfer(SSL *ssl)
+{
+    char buf[MAX_BUF_SIZE] = {0};
+    int ret;
+    ret = SSL_write(ssl, MSG_FOR_SERV, sizeof(MSG_FOR_SERV));
+    if (ret <= 0) {
+        printf("SSL_write failed ret=%d\n", ret);
+        return -1;
+    }
+    printf("SSL_write[%d] sent %s\n", ret, MSG_FOR_SERV);
+
+    ret = SSL_read(ssl, buf, sizeof(buf) - 1);
+    if (ret <= 0) {
+        printf("SSL_read failed ret=%d\n", ret);
+        return -1;
+    }
+    printf("SSL_read[%d] %s\n", ret, buf);
+    return 0;
+}
+
+int tls13_client()
+{
+    SSL_CTX *ctx;
+    SSL *ssl = NULL;
+    int fd;
+    int ret;
+
+    ctx = create_context();
+    if (!ctx) {
+        return -1;
+    }
+
+    ssl = create_ssl_object(ctx);
+    if (!ssl) {
+        goto err_handler;
+    }
+
+    fd = SSL_get_fd(ssl);
+
+    ret = SSL_connect(ssl); 
+    if (ret != 1) {
+        printf("SSL connect failed%d\n", ret);
+        goto err_handler;
+    }
+    printf("SSL connect succeeded\n");
+
+    if (do_data_transfer(ssl)) {
+        printf("Data transfer over TLS failed\n");
+        goto err_handler;
+    }
+    printf("Data transfer over TLS succeeded\n");
+    SSL_free(ssl);
+    SSL_CTX_free(ctx);
+    close(fd);
+
+    return 0;
+err_handler:
+    if (ssl) {
+        SSL_free(ssl);
+    }
+    SSL_CTX_free(ctx);
+    close(fd);
+    return -1;
+}
+
+int main()
+{
+    printf("OpenSSL version: %s, %s\n", OpenSSL_version(OPENSSL_VERSION), OpenSSL_version(OPENSSL_BUILT_ON));
+    if (tls13_client()) {
+        printf("TLS12 client connection failed\n");
+        return -1;
+    }
+    return 0;
+}
