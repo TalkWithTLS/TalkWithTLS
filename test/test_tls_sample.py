db2f1174f8304747cd956888e7341fc70d9b2017
raja-ashok <rashok.svks@gmail.com>
2019-02-22 17:00:11 +0530

    Remove testcase level setup and teardown

--- test/test_tls_sample.py
+++ test/test_tls_sample.py
@@ -8,14 +8,19 @@ log_filename = log_dir + '/' + os.path.basename(__file__) + '.txt'
 
 global log_fd;
 
+def TWT_LOG(log_filename, str):
+    fd = open(log_filename, 'a')
+    fd.write(str)
+    fd.close()
+
 def log_std_out_and_err(exe, out, err):
-    log_fd.write(exe + '\n')
-    log_fd.write('-----------------------------------------------\n')
+    TWT_LOG(log_filename, exe + '\n')
+    TWT_LOG(log_filename, '-----------------------------------------------\n')
     if out != None:
-        log_fd.write(str(out) + '\n')
+        TWT_LOG(log_filename, str(out) + '\n')
     if err != None:
-        log_fd.write('-----------------------------------------------\n')
-        log_fd.write(str(err) + '\n')
+        TWT_LOG(log_filename, '-----------------------------------------------\n')
+        TWT_LOG(log_filename, str(err) + '\n')
 
 def log_procs(proc1, proc2):
     (out, err) = proc1.communicate()
@@ -24,27 +29,22 @@ def log_procs(proc1, proc2):
     log_std_out_and_err('Client', out, err)
 
 def log_tc(apps):
-    log_fd.write('===============================================\n')
-    log_fd.write('Running ' + apps[0]  + ' vs ' + apps[1] + ' ...\n')
-    log_fd.write('===============================================\n')
+    TWT_LOG(log_filename, '===============================================\n')
+    TWT_LOG(log_filename, 'Running ' + apps[0]  + ' vs ' + apps[1] + ' ...\n')
+    TWT_LOG(log_filename, '===============================================\n')
 
 def validate_tc_result(ret1, ret2):
-    log_fd.write('===============================================\n')
     if ret1 != 0 or ret2 != 0:
-        log_fd.write('FAILED !!!!\n')
+        TWT_LOG(log_filename, '###FAILED !!!!\n\n')
         result = -1
     else:
-        log_fd.write('Succeeded\n')
+        TWT_LOG(log_filename, '###Succeeded\n\n')
         result = 0
-    log_fd.write('===============================================\n')
     return result
-
-def TWT_LOG(str):
-    log_fd.write(str)
 
 def validate_app(app):
     if not os.path.isfile(bin_dir + '/' + app):
-        TWT_LOG('Test Apps [' + bin_dir + '/' + app + '] not found !!!\n')
+        TWT_LOG(log_filename, 'Test Apps [' + bin_dir + '/' + app + '] not found !!!\n')
         return -1
     return 0
 
@@ -64,14 +64,6 @@ def run_serv_clnt_app(apps):
     log_procs(proc1, proc2)
     return validate_tc_result(ret1, ret2)
 
-def tc_setup():
-    global log_fd
-    log_fd = open(log_filename, 'a')
-
-def tc_teardown():
-    global log_fd
-    log_fd.close()
-
 # Test Sample Code
 t12_testcases = [
                     ['openssl_tls12_server', 'openssl_tls12_client'],
@@ -79,10 +71,8 @@ t12_testcases = [
                 ]
 
 def test_tls12_sample_code():
-    tc_setup()
     for apps in t12_testcases:
         assert run_serv_clnt_app(apps) == 0
-    tc_teardown()
 
 t13_testcases = [
                     ['openssl_tls13_server', 'openssl_tls13_client'],
@@ -91,7 +81,5 @@ t13_testcases = [
                 ]
 
 def test_tls13_sample_code():
-    tc_setup()
     for apps in t13_testcases:
         assert run_serv_clnt_app(apps) == 0
-    tc_teardown()
