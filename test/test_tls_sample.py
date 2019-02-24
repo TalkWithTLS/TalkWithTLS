332923e0acac0daa30ea92ccb824044404ac8b13
raja-ashok <rashok.svks@gmail.com>
2019-02-24 13:02:24 +0530

    Update testscript to use Pixture and Paramertize features of Pytest

--- test/test_tls_sample.py
+++ test/test_tls_sample.py
@@ -8,25 +8,23 @@ filename = os.path.basename(__file__)
 
 @pytest.fixture
 def tc_setup():
+    '''Initializes log file in TWT log module'''
     TWT_set_log_filename(filename)
 
-t12_testcases = [
-                    ['openssl_tls12_server', 'openssl_tls12_client'],
-                    ['openssl_tls12_verify_cb_server', 'openssl_tls12_verify_cb_client'],
-                ]
+@pytest.mark.parametrize("t12_apps", [
+    (['openssl_tls12_server', 'openssl_tls12_client']),
+    (['openssl_tls12_verify_cb_server', 'openssl_tls12_verify_cb_client']),
+])
 
-# Test TLS1.2 Sample Code
-def test_tls12_sample_code(tc_setup):
-    for apps in t12_testcases:
-        assert run_serv_clnt_app(apps) == 0
+def test_tls12_sample_code(tc_setup, t12_apps):
+    run_serv_clnt_app(t12_apps) == 0
 
-t13_testcases = [
-                    ['openssl_tls13_server', 'openssl_tls13_client'],
-                    #['openssl_tls13_dhe_server', 'openssl_tls13_dhe_client'],
-                    ['wolfssl_tls13_server', 'wolfssl_tls13_client']
-                ]
+@pytest.mark.parametrize("t13_apps", [
+    (['openssl_tls13_server', 'openssl_tls13_client']),
+    #(['openssl_tls13_dhe_server', 'openssl_tls13_dhe_client']),
+    (['wolfssl_tls13_server', 'wolfssl_tls13_client']),
+])
 
-# Test TLS1.3 Sample Code
-def test_tls13_sample_code(tc_setup):
-    for apps in t13_testcases:
-        assert run_serv_clnt_app(apps) == 0
+def test_tls13_sample_code(tc_setup, t13_apps):
+    run_serv_clnt_app(t13_apps) == 0
+
