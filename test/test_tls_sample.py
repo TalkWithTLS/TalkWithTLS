382c8fc6cd480754e4acc6bb6ff8d3380abde5da
raja-ashok <rashok.svks@gmail.com>
2019-03-08 18:58:28 +0530

    Add interop testcases

--- test/test_tls_sample.py
+++ test/test_tls_sample.py
@@ -22,10 +22,17 @@ def test_tls12_sample_code(tc_setup, t12_apps):
 
 @pytest.mark.parametrize("t13_apps", [
     (['openssl_tls13_server', 'openssl_tls13_client']),
-    #(['openssl_tls13_dhe_server', 'openssl_tls13_dhe_client']),
+    #(['openssl_tls13_dhe_server', 'openssl_tls13_dhe_client']), #TODO ffdhe PR is not yet merged
     (['wolfssl_tls13_server', 'wolfssl_tls13_client']),
 ])
 
 def test_tls13_sample_code(tc_setup, t13_apps):
     assert run_serv_clnt_app(t13_apps) == 0
 
+@pytest.mark.parametrize("t13_interop_apps", [
+    #(['openssl_tls13_server', 'wolfssl_tls13_client']), #TODO Failing
+    (['wolfssl_tls13_server', 'openssl_tls13_client']),
+])
+
+def test_tls13_sample_code_interop(tc_setup, t13_interop_apps):
+    assert run_serv_clnt_app(t13_interop_apps) == 0
