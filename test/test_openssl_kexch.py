a7cd7d75b009e61993b754fd0c48f6dcaee1ee81
raja-ashok <rashok.svks@gmail.com>
2019-03-14 11:00:40 +0530

    FFDHE is currently not supported in OpenSSL 1.1.1

--- test/test_openssl_kexch.py
+++ test/test_openssl_kexch.py
@@ -13,7 +13,7 @@ def tc_setup():
 
 @pytest.mark.parametrize("kexch_apps_and_args", [
     (['test_openssl', 'test_openssl', '-S -k 1 -m', '-k 1 m']), #Test with all ECDHE alg
-    (['test_openssl', 'test_openssl', '-S -k 2 -m', '-k 2 m']), #Test with all FFDHE alg
+    #(['test_openssl', 'test_openssl', '-S -k 2 -m', '-k 2 m']), #Test with all FFDHE alg
 ])
 
 def test_openssl_kexch(tc_setup, kexch_apps_and_args):
