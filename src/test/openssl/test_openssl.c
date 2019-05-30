cca28199dc55989211accb8dd6d09231d11d5e17
raja-ashok <rashok.svks@gmail.com>
2019-05-30 14:24:26 +0530

    Restructure cert config code

--- src/test/openssl/test_openssl.c
+++ src/test/openssl/test_openssl.c
@@ -9,11 +9,16 @@
 #include "test_openssl_common.h"
 #include "test_openssl_arg.h"
 #include "test_openssl_kexch.h"
+#include "test_openssl_auth.h"
 
 int tc_conf_update(TC_CONF *conf)
 {
     if (tc_conf_kexch(conf)) {
         printf("TC conf for kexch failed\n");
+        return -1;
+    }
+    if (tc_conf_auth(conf)) {
+        printf("TC conf for authentication failed\n");
         return -1;
     }
     return 0;
--- src/test/openssl/test_openssl_arg.c
+++ src/test/openssl/test_openssl_arg.c
@@ -2,29 +2,6 @@
 #include "test_openssl_common.h"
 #include "test_openssl_arg.h"
 
-void update_certs(TC_CONF *conf)
-{
-    if (conf->server) {
-        conf->cert = EC256_SERVER_CERT_FILE;
-        conf->cert_type = SSL_FILETYPE_PEM;
-        conf->priv_key = EC256_SERVER_KEY_FILE;
-        conf->priv_key_type = SSL_FILETYPE_ASN1;
-        if ((conf->auth & TC_CONF_CLIENT_CERT_AUTH) != 0) {
-            conf->cafiles[0] = EC256_CAFILE1;
-            conf->cafiles_count = 1;
-        }
-    } else {
-        if ((conf->auth & TC_CONF_CLIENT_CERT_AUTH) != 0) {
-            conf->cert = EC256_CLIENT_CERT_FILE;
-            conf->cert_type = SSL_FILETYPE_PEM;
-            conf->priv_key = EC256_CLIENT_KEY_FILE;
-            conf->priv_key_type = SSL_FILETYPE_ASN1;
-        }
-        conf->cafiles[0] = EC256_CAFILE1;
-        conf->cafiles_count = 1;
-    }
-}
-
 void usage()
 {
     printf("-h      - Help\n");
@@ -96,6 +73,5 @@ int parse_arg(int argc, char *argv[], TC_CONF *conf)
         }
     }
 
-    update_certs(conf);
     return 0;
 }
