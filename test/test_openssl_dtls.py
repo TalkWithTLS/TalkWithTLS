e049f777126596d1fc4883f56f8c7df1c2e859a4
raja-ashok <rashok.svks@gmail.com>
2019-08-19 21:03:56 +0530

    Add basic testcase for DTLS

---
+++ test/test_openssl_dtls.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import pytest
+
+from common import *
+
+filename = os.path.basename(__file__)
+
+@pytest.fixture
+def tc_setup():
+    '''Initializes log file in TWT log module'''
+    TWT_set_log_filename(filename)
+
+@pytest.mark.parametrize("app1, app2, app1_args, app2_args, app1_res, app2_res", [
+    # Test DTLS 1.0
+    ('test_openssl', 'test_openssl', ' -S -V 910', ' -V 910 ', 0, 0),
+    # Test DTLS 1.2
+    ('test_openssl', 'test_openssl', ' -S -V 912', ' -V 912 ', 0, 0),
+])
+
+def test_openssl_dtls_basic(tc_setup, app1, app2, app1_args, app2_args, app1_res, app2_res):
+    assert run_serv_clnt_app([app1, app2, app1_args, app2_args, app1_res, app2_res]) == 0
