415e190ae31fa4ccccd8708cd9ef354adb891a93
raja-ashok <rashok.svks@gmail.com>
2019-10-28 20:06:52 +0530

    Update help and arg values

--- test/test_openssl_auth.py
+++ test/test_openssl_auth.py
@@ -12,7 +12,7 @@ def tc_setup():
     TWT_set_log_filename(filename)
 
 @pytest.mark.parametrize("app1, app2, app1_args, app2_args, app1_res, app2_res", [
-    ('test_openssl', 'test_openssl', ' --serv --cauth --msgcb ', ' --cauth --msgcb ', 0, 0),
+    ('test_openssl', 'test_openssl', ' -serv -cauth -msgcb ', ' -cauth -msgcb ', 0, 0),
 ])
 
 def test_openssl_tls13_auth(tc_setup, app1, app2, app1_args, app2_args, app1_res, app2_res):
--- test/test_openssl_key_update.py
+++ test/test_openssl_key_update.py
@@ -14,13 +14,13 @@ def tc_setup():
 @pytest.mark.parametrize("app1, app2, app1_args, app2_args, app1_res, app2_res", [
     # Server initiates key update after first handshake and data transfer.
     # And perfroms data transfer again
-    ('test_openssl', 'test_openssl', ' --serv --kupda 1 --msgcb ', ' --kupda 1 --msgcb ', 0, 0),
+    ('test_openssl', 'test_openssl', ' -serv -kupda 1 -msgcb ', ' -kupda 1 -msgcb ', 0, 0),
     # Key update on client
-    ('test_openssl', 'test_openssl', ' --serv --kupda 2 --msgcb ', ' --kupda 2 --msgcb ', 0, 0),
+    ('test_openssl', 'test_openssl', ' -serv -kupda 2 -msgcb ', ' -kupda 2 -msgcb ', 0, 0),
     # Non request key update on server
-    ('test_openssl', 'test_openssl', ' --serv --kupda 3 --msgcb ', ' --kupda 3 --msgcb ', 0, 0),
+    ('test_openssl', 'test_openssl', ' -serv -kupda 3 -msgcb ', ' -kupda 3 -msgcb ', 0, 0),
     # Non request key update on client 
-    ('test_openssl', 'test_openssl', ' --serv --kupda 4 --msgcb ', ' --kupda 4 --msgcb ', 0, 0),
+    ('test_openssl', 'test_openssl', ' -serv -kupda 4 -msgcb ', ' -kupda 4 -msgcb ', 0, 0),
 ])
 
 def test_openssl_kupdate(tc_setup, app1, app2, app1_args, app2_args, app1_res, app2_res):
