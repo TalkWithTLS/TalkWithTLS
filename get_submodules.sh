0bcc9c749ccd04dd4507c6e70848a7f3d4976ff4
raja-ashok <rashok.svks@gmail.com>
2020-04-02 19:01:33 +0530

    Update submodule pull scripts

--- get_submodules.sh
+++ get_submodules.sh
@@ -10,24 +10,7 @@ trap 'err_report $LINENO' ERR
 
 git submodule init
 git submodule update
+git submodule foreach git pull origin master 1>/dev/null 2>&1
+echo "Pulled latest code from origin"
 echo ""
-
-DEPENDENCY_DIR=dependency
-
-declare -a dep_modules=('openssl-master'
-                        'openssl-master-rel'
-                        'wolfssl-master')
-
-for module in "${dep_modules[@]}"
-do
-    echo "Updating git submodule ${DEPENDENCY_DIR}/${module}..."
-    cd ${DEPENDENCY_DIR}/${module}
-    if [ $? -eq 0 ]; then
-        git remote update > /dev/null
-        git checkout master > /dev/null
-        git pull origin master > /dev/null
-        git log -n 1 --oneline
-        cd -
-    fi
-    echo ""
-done
+git submodule foreach git log -n 1 --oneline
