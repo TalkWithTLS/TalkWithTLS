5a35b3db0c5f4702d6ec880675d0b1a48a4e9579
raja-ashok <rashok.svks@gmail.com>
2020-05-06 14:29:59 +0530

    Automate TLS1.3 resumption

---
+++ test/test_tls13_resumption.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import pytest
+
+from common import *
+
+filename = os.path.basename(__file__)
+
+@pytest.fixture
+def tc_setup():
+    '''Initializes log file in TWT log module'''
+    TWT_set_log_filename(filename)
+
+@pytest.mark.parametrize("sarg, carg", [
+    (' -serv -res ', ' -res ')
+])
+
+def test_t13_resumption(tc_setup, sarg, carg):
+    run_test(inspect.stack()[0][3], sarg, carg)
